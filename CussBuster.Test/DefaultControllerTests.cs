2e2283c940b258b1a1f731c2904975722cd464e7
iduckworth <ian.duckworth@levvel.io>
2018-05-28 09:41:11 -0400

    Fully built out unit test suite

---
+++ CussBuster.Test/DefaultControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using CussBuster.Controllers;
+using CussBuster.Core.Helpers;
+using CussBuster.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CussBuster.Test
+{
+	[TestFixture]
+    public class DefaultControllerTests
+    {
+		private DefaultController _defaultController;
+		private Mock<IMainHelper> _mainHelper;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_mainHelper = new Mock<IMainHelper>();
+
+			_defaultController = new DefaultController(_mainHelper.Object);
+		}
+
+		[Test]
+		public void Post_BadAuthentication()
+		{
+			const string authToken = "testAuthToken";
+
+			//arrange
+			_mainHelper.Setup(x => x.CheckAuthorization(authToken)).Returns(false);
+
+			//act
+			var result = _defaultController.Post("test text", authToken) as UnauthorizedResult;
+
+			//assert
+			Assert.True(result != null);
+		}
+
+		[Test]
+		public void Post_BadRequest()
+		{
+			const string authToken = "testAuthToken";
+
+			//arrange
+			_mainHelper.Setup(x => x.CheckAuthorization(authToken)).Throws(new Exception("test exception"));
+
+			//act
+			var result = _defaultController.Post("test text", authToken) as BadRequestResult;
+
+			//assert
+			Assert.True(result != null);
+		}
+
+		[Test]
+		public void Post_Ok()
+		{
+			const string authToken = "testAuthToken";
+			const string text = "test text";
+
+			//arrange
+			_mainHelper.Setup(x => x.CheckAuthorization(authToken)).Returns(true);
+			_mainHelper.Setup(x => x.FindMatches(text)).Returns(new List<ReturnModel>
+			{
+				new ReturnModel
+				{
+					Occurrences = 1,
+					Severity = 1,
+					Word = "text",
+					WordType = "Vulgarity",
+					WordTypeId = 1
+				}
+			});
+
+			//act
+			var result = _defaultController.Post(text, authToken) as OkObjectResult;
+
+			//assert
+			Assert.True(result != null);
+
+			var value = (result.Value as IEnumerable<ReturnModel>).First();
+
+			Assert.True(value.Occurrences == 1);
+			Assert.True(value.Severity == 1);
+			Assert.True(value.Word == "text");
+			Assert.True(value.WordType == "Vulgarity");
+			Assert.True(value.WordTypeId == 1);
+		}
+    }
+}
--- CussBuster.Test/MainHelperTests.cs
+++ CussBuster.Test/MainHelperTests.cs
@@ -4,6 +4,7 @@ using CussBuster.Core.Helpers;
 using CussBuster.Core.Models;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -54,6 +55,112 @@ namespace CussBuster.Test
 		}
 
 		[Test]
+		public void FindMatches_SingleMatch_CaseMismatch()
+		{
+			//arrange
+			_wordLoader.Setup(x => x.Load()).Returns(new List<WordModel>
+			{
+				new WordModel
+				{
+					Word = "test",
+					SearchTypeId = 1,
+					Severity = 1,
+					WordTypeId = 1
+				}
+			});
+
+			//act
+			var result = _mainHelper.FindMatches("this is a TEST");
+
+			//assert
+			var item = result.First();
+
+			Assert.True(result.Count() == 1);
+			Assert.True(item.Occurrences == 1);
+			Assert.True(item.Severity == 1);
+			Assert.True(item.Word == "test");
+			Assert.True(item.WordType == "Vulgarity");
+		}
+
+		[Test]
+		public void FindMatches_SingleMatch_EqualsSearch_Negative()
+		{
+			//arrange
+			_wordLoader.Setup(x => x.Load()).Returns(new List<WordModel>
+			{
+				new WordModel
+				{
+					Word = "test",
+					SearchTypeId = 1,
+					Severity = 1,
+					WordTypeId = 1
+				}
+			});
+
+			//act
+			var result = _mainHelper.FindMatches("we are testing");
+
+			//assert
+			Assert.True(result.Count() == 0);
+		}
+
+		[Test]
+		public void FindMatches_SingleMatch_ContainsSearch()
+		{
+			//arrange
+			_wordLoader.Setup(x => x.Load()).Returns(new List<WordModel>
+			{
+				new WordModel
+				{
+					Word = "test",
+					SearchTypeId = 2,
+					Severity = 1,
+					WordTypeId = 1
+				}
+			});
+
+			//act
+			var result = _mainHelper.FindMatches("we are testing");
+
+			//assert
+			var item = result.First();
+
+			Assert.True(result.Count() == 1);
+			Assert.True(item.Occurrences == 1);
+			Assert.True(item.Severity == 1);
+			Assert.True(item.Word == "testing");
+			Assert.True(item.WordType == "Vulgarity");
+		}
+
+		[Test]
+		public void FindMatches_SingleMatch_MultipleOccurances()
+		{
+			//arrange
+			_wordLoader.Setup(x => x.Load()).Returns(new List<WordModel>
+			{
+				new WordModel
+				{
+					Word = "test",
+					SearchTypeId = 1,
+					Severity = 1,
+					WordTypeId = 1
+				}
+			});
+
+			//act
+			var result = _mainHelper.FindMatches("this is a test test");
+
+			//assert
+			var item = result.First();
+
+			Assert.True(result.Count() == 1);
+			Assert.True(item.Occurrences == 2);
+			Assert.True(item.Severity == 1);
+			Assert.True(item.Word == "test");
+			Assert.True(item.WordType == "Vulgarity");
+		}
+
+		[Test]
 		public void FindMatches_NoMatches()
 		{
 			//arrange
@@ -74,5 +181,47 @@ namespace CussBuster.Test
 			//assert
 			Assert.True(result.Count() == 0);
 		}
-    }
+
+		[Test]
+		public void CheckAuthorization_NonGuid()
+		{
+			//arrange / act
+			var result = _mainHelper.CheckAuthorization("test");
+
+			//assert
+			Assert.True(result == false);
+		}
+
+		[Test]
+		public void CheckAuthorization_NonExistantGuid()
+		{
+			var guidString = "ab9767dd-3c56-4750-a669-76564e057f83";
+			var guid = new Guid(guidString);
+
+			//arrange
+			_authChecker.Setup(x => x.CheckToken(guid)).Returns(false);
+
+			//act
+			var result = _mainHelper.CheckAuthorization(guidString);
+
+			//assert
+			Assert.True(result == false);
+		}
+
+		[Test]
+		public void CheckAuthorization_ExistingGuid()
+		{
+			var guidString = "ab9767dd-3c56-4750-a669-76564e057f83";
+			var guid = new Guid(guidString);
+
+			//arrange
+			_authChecker.Setup(x => x.CheckToken(guid)).Returns(true);
+
+			//act
+			var result = _mainHelper.CheckAuthorization(guidString);
+
+			//assert
+			Assert.True(result == true);
+		}
+	}
 }
